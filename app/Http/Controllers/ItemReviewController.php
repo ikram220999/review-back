33abfa4ddd27f5294cb6b591d9980b4fca8e31c1
LAPTOP-K0PEIKNF\ikram <lalatputih22@gmail.com>
2022-08-06 04:25:42 +0800

    first commit

---
+++ app/Http/Controllers/ItemReviewController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item_review;
+use Illuminate\Http\Request;
+
+class ItemReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Item_review  $item_review
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Item_review $item_review)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Item_review  $item_review
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Item_review $item_review)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Item_review  $item_review
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Item_review $item_review)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Item_review  $item_review
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Item_review $item_review)
+    {
+        //
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory;
+use Illuminate\Support\Arr;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        $locales = ['en'];
+
+        $faker = Factory::create(Arr::random($locales));
+
+        DB::table('categories')->insert([
+            'name' => "Mobile Phone",
+        ]);
+        DB::table('categories')->insert([
+            'name' => "Laptop & PC",
+
+        ]);
+        DB::table('categories')->insert([
+            'name' => "Furniture",
+        ]);
+        DB::table('categories')->insert([
+            'name' => "Food and Drink",
+        ]);
+
+        DB::table('items')->insert([
+            'name' => "Iphone 11",
+            'description' => $faker->realText,
+            'price' => "2300.00",
+            'category_id' => "1",
+        ]);
+
+        DB::table('items')->insert([
+            'name' => "Iphone 13 Pro Max",
+            'description' => $faker->realText,
+            'price' => "5800.00",
+            'category_id' => "1",
+        ]);
+
+        DB::table('items')->insert([
+            'name' => "Asus ROG Gaming Laptop",
+            'description' => $faker->realText,
+            'price' => "4099.00",
+            'category_id' => "2",
+        ]);
+
+        DB::table('items')->insert([
+            'name' => "Nasi Kandar Subaidah",
+            'description' => $faker->realText,
+            'price' => "10.00",
+            'category_id' => "4",
+        ]);
+
+        DB::table('items')->insert([
+            'name' => "Kerusi Gaming Todak",
+            'description' => $faker->realText,
+            'price' => "1200.00",
+            'category_id' => "3",
+        ]);
+
+    }
+}
